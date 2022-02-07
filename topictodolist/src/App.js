6ba66094517cb359973ef5d073e06625bf88bc58
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-02-07 18:16:18 +0900

    Components Cre

--- topictodolist/src/App.js
+++ topictodolist/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import World from './components/World';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,6 +16,7 @@ function App() {
         >
           Learn React
         </a>
+        <World/>
       </header>
     </div>
   );
---
+++ topictodolist/src/components/World.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function World() {
+    return (
+        <>
+            <h1>World Page.</h1>
+        </>
+    )
+}
